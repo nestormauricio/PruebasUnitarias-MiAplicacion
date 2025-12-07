6ae297c00951b6b2f18e5bfc37d71b61dd4522b8
Nestor Mauricio Pérez Ruíz <nestormauricio@live.com>
2025-12-07 12:35:51 -0500

    Primer commit recién generado el proyecto y antes de crear la rama

---
+++ MiAplicacion.Tests/CalculadoraTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiAplicacion.Tests
+{
+    internal class CalculadoraTests
+    {
+
+        [Fact]
+        public void Sumar_DosNumeros_RetornaResultadoCorrecto()
+        {
+            var calc = new Calculadora();
+            var resultado = calc.Sumar(2, 3);
+            Assert.Equal(5, resultado);
+        }
+
+        [Fact]
+        public void Restar_DosNumeros_RetornaResultadoCorrecto()
+        {
+            var calc = new Calculadora();
+            var resultado = calc.Restar(10, 4);
+            Assert.Equal(6, resultado);
+        }
+
+        [Theory]
+        [InlineData(2, true)]
+        [InlineData(3, false)]
+        [InlineData(10, true)]
+        public void EsPar_ValidaNumeros(int numero, bool esperado)
+        {
+            var calc = new Calculadora();
+            var resultado = calc.EsPar(numero);
+            Assert.Equal(esperado, resultado);
+        }
+    }
+}
---
+++ MiAplicacion/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiAplicacion
+{
+    internal class Calculadora
+    {
+        public int Sumar(int a, int b) => a + b;
+        public int Restar(int a, int b) => a - b;
+        public bool EsPar(int numero) => numero % 2 == 0;
+    }
+}
